ca5fbe1a7a396c3f82673a39b2cbe8470d56c8b8
Alias26 <khmjuhee979@gmail.com>
2022-11-22 18:09:06 +0900

    add: streamlit predict

--- mmdetection/app.py
+++ mmdetection/app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 from pycocotools.coco import COCO
-from mmdet.datasets.pipelines import Compose, Albu
-from configs._trash_._base_.datasets.transforms import train_pipeline, albu_train_transforms
+from mmdet.datasets import (build_dataset, build_dataloader)
+from mmdet.apis import init_detector, inference_detector, show_result_pyplot
+from mmcv import Config
+
 import os
 import cv2
 import numpy as np
-from PIL import Image, ImageDraw
-from mmdet.datasets import build_dataset
-from mmcv import Config
+import copy
 
 st.set_page_config(layout='wide')
 
@@ -23,16 +23,29 @@ colors = [(102, 147, 171), (247, 215, 105), (37, 94, 150),
  (241, 103, 66), (162, 217, 227), (241, 211, 203),
  (159, 186, 121), (213, 69, 132), (109, 127, 91), (214, 183, 159)]
 
-cfg = Config.fromfile('./configs/_trash_/faster_rcnn_r50_fpn.py')
+cfg_path = './configs/_trash_/faster_rcnn_r50_fpn.py'
+cfg = Config.fromfile(cfg_path)
 
 
 
-@st.cache
+@st.cache(allow_output_mutation=True)
 def load_data():
     ann_file = root + 'train.json'
     origin_dataset = COCO(ann_file)
     train_dataset = build_dataset(cfg.data.train)
+
     return origin_dataset, train_dataset
+
+# @st.cache(hash_funcs=MyModelClass: lambda model: model.get_config()})
+def load_model():
+    epoch = 'epoch_12'
+
+    work_dir = './work_dirs/faster_rcnn_r50_fpn'
+    checkpoint_path = os.path.join(work_dir, f'{epoch}.pth')
+
+    model = model = init_detector(cfg_path, checkpoint_path) # build detector
+
+    return model
 
 
 def get_img_infos(dataset):
@@ -101,7 +114,7 @@ def make_bbox(image, state):
     return image
 
 
-def make_bbox_image(dataset, image, id, tab_bbox):
+def make_bbox_image(dataset, origin_image, id, tab_bbox):
     tab_bbox_col1, tab_bbox_col2 = tab_bbox.columns(2)
 
     state = create_state(dataset, id)
@@ -109,6 +122,7 @@ def make_bbox_image(dataset, image, id, tab_bbox):
     make_check_box(tab_bbox_col2, state['labels'], state['viz'], state['label_viz'])
     make_multi_select_box(tab_bbox, state['boxes_id'], state['viz'])
 
+    image = copy.deepcopy(origin_image)
     image = make_bbox(image, state)
     image = cv2.resize(image, (512, 512))
 
@@ -117,27 +131,35 @@ def make_bbox_image(dataset, image, id, tab_bbox):
     return image
 
 
-def make_aug_image(aug_datasets, tab_augmentation, index, bbox_image) -> None:
+def make_aug_image(aug_datasets, tab_augmentation, index, origin_image) -> None:
     tab_augmentation_col1, tab_augmentation_col2 = tab_augmentation.columns(2)
 
     aug_image = aug_datasets[index]['img'].data
     aug_image = aug_image.permute(1,2,0)
     aug_image = np.ascontiguousarray(aug_image.data.cpu())
     
-    # boxes = aug_datasets[index]['gt_bboxes'].data.numpy().astype(int)
-    # labels = aug_datasets[index]['gt_labels'].data.numpy()
-
-    # aug_image = make_bbox(aug_image, boxes, labels)
-    tab_augmentation_col1.image(bbox_image, caption='Original Image')
+    tab_augmentation_col1.image(origin_image, caption='Original Image')
     tab_augmentation_col2.image(aug_image, caption="Augmentation Image", clamp=True)
+
+
+def make_pred_image(model, tab_predict, origin_image, bbox_image) -> None:
+    tab_predict_col1, tab_predict_col2 = tab_predict.columns(2)
+    
+    output = inference_detector(model, origin_image)
+    # predict_image = show_result_pyplot(model, origin_image, output, score_thr=0.5)
+    predict_image = model.show_result(origin_image, output)
+
+    tab_predict_col1.image(bbox_image, caption='GT_Bboxes')
+    tab_predict_col2.image(predict_image)
 
 
 def main():
     st.title('Trash Object Detection')
 
-    if 'dataset' not in st.session_state and 'img_infos' not in st.session_state:
+    if 'dataset' not in st.session_state:
         st.session_state.dataset, st.session_state.aug_dataset = load_data()
         st.session_state.img_infos = get_img_infos(st.session_state.dataset)
+        st.session_state.model = load_model()
 
     file_names = [img_info['file_name'] for img_info in st.session_state.img_infos]
     file_name = st.selectbox('Select Image', file_names, index=0)
@@ -149,7 +171,8 @@ def main():
     origin_image = cv2.imread(os.path.join(root, file_name))
 
     bbox_image = make_bbox_image(st.session_state.dataset, origin_image, file_id, tab_bbox)
-    make_aug_image(st.session_state.aug_dataset, tab_augmentation, file_id, bbox_image)
+    make_aug_image(st.session_state.aug_dataset, tab_augmentation, file_id, origin_image)
+    make_pred_image(st.session_state.model, tab_prediction, origin_image, bbox_image)
 
 
 main()
