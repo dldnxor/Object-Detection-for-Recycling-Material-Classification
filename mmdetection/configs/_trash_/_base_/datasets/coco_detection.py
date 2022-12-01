ec2d1ae9259a1007fb2ff3441d8bcfb80a121329
Alias26 <khmjuhee979@gmail.com>
2022-12-01 18:23:08 +0900

    update:dataset configs

--- mmdetection/configs/_trash_/_base_/datasets/coco_detection.py
+++ mmdetection/configs/_trash_/_base_/datasets/coco_detection.py
@@ -14,14 +14,54 @@ classes = (
     "Battery",
     "Clothing",
 )
-img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+img_norm_cfg = dict(
+    mean=[123.65067045, 117.3973029, 110.0754267],
+    std=[54.034579050000005, 53.36968695, 54.78390675],
+    to_rgb=True,
+)
+albu_train_transforms = [
+    dict(type="Blur", blur_limit=4, p=0.5),
+    dict(
+        type="RandomBrightnessContrast",
+        brightness_limit=0.15,
+        contrast_limit=0.2,
+        brightness_by_max=False,
+        p=0.5,
+    ),
+    dict(
+        type="ShiftScaleRotate",
+        rotate_limit=90,
+        p=0.5,
+    ),
+]
 train_pipeline = [
-    dict(type="LoadImageFromFile"),
-    dict(type="LoadAnnotations", with_bbox=True),
+    # dict(type="LoadImageFromFile"),
+    # dict(type="LoadAnnotations", with_bbox=True),
+    dict(
+        type="Mosaic",
+        img_scale=(1024, 1024),
+        center_ratio_range=(0.9, 1.1),
+        pad_val=114.0,
+        prob=0.6,
+    ),
     dict(type="Resize", img_scale=(512, 512), keep_ratio=True),
     dict(type="RandomFlip", flip_ratio=0.5),
     dict(type="Normalize", **img_norm_cfg),
     dict(type="Pad", size_divisor=32),
+    dict(
+        type="Albu",
+        transforms=albu_train_transforms,
+        bbox_params=dict(
+            type="BboxParams",
+            format="pascal_voc",
+            label_fields=["gt_labels"],
+            min_visibility=0.0,
+            filter_lost_elements=True,
+        ),
+        keymap={"img": "image", "gt_bboxes": "bboxes"},
+        update_pad_shape=False,
+        skip_img_without_anno=True,
+    ),
     dict(type="DefaultFormatBundle"),
     dict(type="Collect", keys=["img", "gt_bboxes", "gt_labels"]),
 ]
@@ -46,10 +86,15 @@ data = dict(
     samples_per_gpu=2,
     workers_per_gpu=2,
     train=dict(
-        type=dataset_type,
-        ann_file=data_root + "train_split.json",
-        img_prefix=data_root,
-        classes=classes,
+        type="MultiImageMixDataset",
+        dataset=dict(
+            type="CocoDataset",
+            ann_file=data_root + "train_split.json",
+            img_prefix=data_root,
+            classes=classes,
+            pipeline=[dict(type="LoadImageFromFile"), dict(type="LoadAnnotations", with_bbox=True)],
+            filter_empty_gt=False,
+        ),
         pipeline=train_pipeline,
     ),
     val=dict(
